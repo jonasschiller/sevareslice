b3de09a6a527934c1dc1097b5f8d43c78f2aa2e7
chart21 <chart21@web.de>
2023-06-03 21:22:40 +0200

    deactivated new options

--- host_scripts/host_setup.sh
+++ host_scripts/host_setup.sh
@@ -38,19 +38,21 @@ apt install -y automake build-essential git libboost-dev libboost-thread-dev par
     libntl-dev libsodium-dev libssl-dev libtool m4 python3 texinfo yasm linux-cpupower \
     python3-pip time iperf3 \
     software-properties-common
-echo 'deb http://deb.debian.org/debian testing main' > /etc/apt/sources.list.d/testing.list
-apt update -y
 # wget https://apt.llvm.org/llvm.sh
 # chmod +x llvm.sh
 # ./llvm.sh -y 15
 # apt install -y clang-15 gcc-12 g++-12
-apt install -y gcc-12 g++-12
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.4/clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04.tar.xz
-tar -xvf clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04.tar.xz
-mv clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04 /usr/local/llvm-16
-echo 'export PATH="/usr/local/llvm-16/bin:$PATH"' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH="/usr/local/llvm-16/lib:$LD_LIBRARY_PATH"' >> ~/.bashrc
-source ~/.bashrc
+
+
+# echo 'deb http://deb.debian.org/debian testing main' > /etc/apt/sources.list.d/testing.list
+# apt update -y
+# apt install -y gcc-12 g++-12
+# wget https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.4/clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04.tar.xz
+# tar -xvf clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04.tar.xz
+# mv clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04 /usr/local/llvm-16
+# echo 'export PATH="/usr/local/llvm-16/bin:$PATH"' >> ~/.bashrc
+# echo 'export LD_LIBRARY_PATH="/usr/local/llvm-16/lib:$LD_LIBRARY_PATH"' >> ~/.bashrc
+# source ~/.bashrc
 
 pip3 install -U numpy
 checkConnection "github.com"
