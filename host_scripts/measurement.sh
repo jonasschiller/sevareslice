abe736bb571f70b8cd878d8abd1a3c4a2e1138e2
chart21 <chart21@web.de>
2024-01-04 11:42:58 +0100

    fixed bug with splitroles

--- host_scripts/measurement.sh
+++ host_scripts/measurement.sh
@@ -145,7 +145,7 @@ if [ "$splitroles" -eq 0 ]; then
         /bin/time -f "$timerf" timeout 1000s ./run-P"$player".o "$ipA" "$ipB" "$ipC" &>> testresults || success=false
     fi
 elif [ "$splitroles" -eq 1 ] && [ "$player" -lt 3 ]; then
-    /bin/time -f "$timerf" timeout 1000s ./scripts/split-roles-3-execute.sh -p "$player" -a "$ipA" -b "$ipB" &>> testresults || success=false
+    /bin/time -f "$timerf" timeout 1000s ./scripts/split-roles-3-execute.sh -p "$player" -a "$ipA" -b "$ipB" -c "$ipC" &>> testresults || success=false
 elif [ "$splitroles" -eq 2 ]; then
     /bin/time -f "$timerf" timeout 1000s ./scripts/split-roles-3to4-execute.sh -p "$player" -a "$ipA" -b "$ipB" -c "$ipC" -d "$ipD" &>> testresults || success=false
 elif [ "$splitroles" -eq 3 ]; then
